4b8fb18c773321e643be2e5f975798b32ab43d59
Raj Kushwaha <41170098+RajTheCreater@users.noreply.github.com>
2025-04-09 13:32:16 +0530

    sd
    Co-Authored-By: Raj Kushwaha <41170098+rajthecreater@users.noreply.github.com>

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class HomeController : Controller
+    {
+        [HttpGet("Index")]
+        [Produces("application/json")]
+        public async Task<ActionResult> Index(int id)
+        {
+            ChannelFactory<IService1> factory = new ChannelFactory<IService1>(new BasicHttpBinding(), new EndpointAddress("http://localhost:8089/Service1.svc"));
+            IService1 proxy = factory.CreateChannel();
+
+            var result = proxy.GetDataAsync(id).Result;
+            var res = (result == null ? string.Empty : result);
+            return Ok(res);
+        }
+        [HttpGet("Details")]
+        [Produces("application/json")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,97 @@
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json;
+using ServiceReference1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "A simple example ASP.NET Core Web API",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Shayne Boyer",
+            Email = string.Empty,
+            Url = new Uri("https://twitter.com/spboyer"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+        }
+    });
+
+    var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+}); app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseEndpoints(routes =>
+{
+    routes.MapControllers();
+    routes.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+app.Run();
